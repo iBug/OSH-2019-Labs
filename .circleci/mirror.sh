568b9c71f75a10df68ecb96aa26e7e8c110d5bcd
iBug <7273074+iBug@users.noreply.github.com>
2019-03-15 14:23:47 +0800

    try new SSH config

--- .circleci/mirror.sh
+++ .circleci/mirror.sh
@@ -9,7 +9,14 @@ fi
 
 echo "$SSH_KEY_E" |
   base64 -d |
-  gunzip -c > ~/.ssh/id_rsa
+  gunzip -c > ~/.ssh/m.id_rsa
 
-git remote add mirror git@github.com:iBug/OSH-2019-Labs.git
+cat >> ~/.ssh/config << EOF
+Host GHMirror
+  HostName github.com
+  User git
+  Port 22
+  IdentityFile ~/.ssh/m.id_rsa
+EOF
+git remote add mirror GHMirror:iBug/OSH-2019-Labs.git
 git push mirror +master
