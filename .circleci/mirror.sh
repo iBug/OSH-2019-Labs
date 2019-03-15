e17e40165d18425a2f32f432f64e4f16978742b9
iBug <7273074+iBug@users.noreply.github.com>
2019-03-15 15:30:27 +0800

    Use GitHub Token

--- .circleci/mirror.sh
+++ .circleci/mirror.sh
@@ -2,23 +2,10 @@
 
 set -e
 
-if [ -z "$SSH_KEY_E" ]; then
-  echo "No SSH key found in environment, set it as \$SSH_KEY_E" >&2
+if [ -z "$GH_TOKEN" ]; then
+  echo "Missing \$GH_TOKEN" >&2
   exit 1
 fi
 
-echo "$SSH_KEY_E" |
-  base64 -d |
-  gunzip -c > ~/.ssh/m.id_rsa
-
-set -x
-
-cat >> ~/.ssh/config << EOF
-Host GHMirror
-  HostName github.com
-  User git
-  Port 22
-  IdentityFile ~/.ssh/m.id_rsa
-EOF
-git remote add mirror GHMirror:iBug/OSH-2019-Labs.git
-git push mirror +master
+git remote add mirror "https://$GH_TOKEN@github.com/iBug/OSH-2019-Labs.git"
+git push -q mirror +master
